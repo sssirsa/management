d21b384b863daf6c56b8c8ab0fc4730bd405fc1c
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-03-10 11:32:44 -0600

    Update for Agency and Condition

---
+++ src/agency/GetAllHandler.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const connectToDatabase = require('../../db');
+const Agency = require('../../models/Agency');
+
+module.exports.getAll = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    try {
+        connectToDatabase()
+        let response = await findAgency()
+        if (response.length == 0) {
+            return {
+                statusCode: 404,
+                headers: { 'Content-Type': 'application/json' },
+                body: 'No hay agencias en la base de datos'
+            }
+        }
+        else {
+            return {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(response)
+            }
+        }
+    }
+    catch (error) {
+        return {
+            statusCode: 500,
+            headers: { 'Content-Type': 'application/json' },
+            body: error.message
+        }
+    }
+}
+
+async function findAgency() {
+    return new Promise(function (resolve, reject) {
+        Agency.find({},
+            function (error, docs) {
+                if (error) {
+                    reject({
+                        statusCode: 500,
+                        body: JSON.stringify(error),
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                }
+                resolve(docs);
+            }
+        ).sort({ 'code': 1 });
+    });
+}
---
+++ src/agency/GetOneHandler.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const connectToDatabase = require('../../db');
+const Agency = require('../../models/Agency');
+
+module.exports.getOne = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    var AgencyId = event.pathParameters.id;
+    try {
+        if (!event || !event.pathParameters || !event.pathParameters.id) {
+            return {
+                statusCode: 400,
+                headers: { 'Content-Type': 'application/json' },
+                body: 'No se ha introducido ningún id para busqueda'
+            }
+        }
+        connectToDatabase()
+        let response = await findAgency(AgencyId)
+        if (response.length == 0) {
+            return {
+                statusCode: 404,
+                headers: { 'Content-Type': 'application/json' },
+                body: 'No se encontro agencia con el id especificado'
+            }
+        }
+        else {
+            return {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(response[0])
+            }
+        }
+    }
+    catch (error) {
+        return {
+            statusCode: 500,
+            headers: { 'Content-Type': 'application/json' },
+            body: error.message
+        }
+    }
+}
+
+async function findAgency(AgencyId) {
+    return new Promise(function (resolve, reject) {
+        Agency.findById(AgencyId,
+            function (error, docs) {
+                if (error) {
+                    reject({
+                        statusCode: 500,
+                        body: JSON.stringify(error),
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                }
+                resolve(docs);
+            }
+        );
+    });
+}
