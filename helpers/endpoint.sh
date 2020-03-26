ae1ecd28614b364df9fad7890c70b7fe1041ade3
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-03-26 13:36:12 -0600

    delete helpers

--- helpers/endpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Assuming you deployed with a 'sls deploy' | tee deploy.out this script
-# will auto fill in the ENDPOINT enviroment variable
-
-node helpers/getEndpoint.js > .build/endpoint.out
-export ENDPOINT = `cat .build/endpoint.out`
-
-echo $ENDPOINT
