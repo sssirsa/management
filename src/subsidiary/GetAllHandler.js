db386580c594a81b22f0dc0c96c30cb64f48e61c
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-03-26 13:35:00 -0600

    subsidiary it's ok

---
+++ src/subsidiary/GetAllHandler.js
@@ -0,0 +1,47 @@
+const connectToDatabase = require('../../db');
+const Subsidiary = require('../../models/Subsidiary');
+
+async function findSubsidiary() {
+    return new Promise(((resolve, reject) => {
+        Subsidiary.find({},
+            (error, docs) => {
+                if (error) {
+                    reject(new Error({
+                        statusCode: 500,
+                        body: JSON.stringify(error),
+                        headers: { 'Content-Type': 'application/json' }
+                    }));
+                }
+                resolve(docs);
+            }
+        ).sort({ 'nombre': 1 });
+    }));
+}
+
+module.exports.getAll = async (event, context) => {
+    const mongoconection = context;
+    mongoconection.callbackWaitsForEmptyEventLoop = false;
+    try {
+        connectToDatabase()
+        const response = await findSubsidiary()
+        if (response.length === 0) {
+            return {
+                statusCode: 404,
+                headers: { 'Content-Type': 'application/json' },
+                body: 'No hay sucursales en la base de datos'
+            }
+        }
+            return {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(response)
+            }
+    }
+    catch (error) {
+        return {
+            statusCode: 500,
+            headers: { 'Content-Type': 'application/json' },
+            body: error.message
+        }
+    }
+}
