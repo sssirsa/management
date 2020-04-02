cc0b6ba313699f94c57a90029156c04d754bee07
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-04-02 15:10:25 -0600

    fix db url connection

--- db.js
+++ db.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 let isConnected
 
+// eslint-disable-next-line no-undef
 module.exports = connectToDatabase = () => {
   if (isConnected) {
     console.log('=> using existing database connection')
     return Promise.resolve()
   }
   console.log('=> using new database connection')
-  return mongoose.connect(process.env.DB)
+  return mongoose.connect(process.env.DB, { useNewUrlParser: true })
     .then(db => {
       isConnected = db.connections[0].readyState
     })
